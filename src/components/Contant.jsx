4dce0bc6eb8b27c591a7434671048bd269cf0530
test <test@example.com>
2024-09-30 09:51:32 +0300

    UpdateTow

--- src/components/Contant.jsx
+++ src/components/Contant.jsx
@@ -13,31 +13,31 @@ export default function Contant(props) {
 
             <div className="w-screen flex flex-row flex-wrap gap-4 absolute pb-5 bottom-0 left-0 justify-center items-center">
                 <Link to="/html">
-                    <div className="bg-gray-700 p-5 rounded-2xl hover:bg-gray-800">
+                    <div className="bg-gray-800 p-5 rounded-2xl hover:bg-gray-900">
                         <h3 className="text-white font-bold text-xl md:text-3xl lg:text-5xl">HTML</h3>
                     </div>
                 </Link>
 
                 <Link to="/css">
-                    <div className="bg-gray-700 p-5 rounded-2xl hover:bg-gray-800">
+                    <div className="bg-gray-800 p-5 rounded-2xl hover:bg-gray-900">
                         <h3 className="text-white font-bold text-xl md:text-3xl lg:text-5xl">CSS</h3>
                     </div>
                 </Link>
 
                 <Link to="/js">
-                    <div className="bg-gray-700 p-5 rounded-2xl hover:bg-gray-800">
+                    <div className="bg-gray-800 p-5 rounded-2xl hover:bg-gray-900">
                         <h3 className="text-white font-bold text-xl md:text-3xl lg:text-5xl">JS</h3>
                     </div>
                 </Link>
 
                 <Link to="/react">
-                    <div className="bg-gray-700 p-5 rounded-2xl hover:bg-gray-800">
+                    <div className="bg-gray-800 p-5 rounded-2xl hover:bg-gray-900">
                         <h3 className="text-white font-bold text-xl md:text-3xl lg:text-5xl">REACT</h3>
                     </div>
                 </Link>
 
                 <Link to="/node">
-                    <div className="bg-gray-700 p-5 rounded-2xl hover:bg-gray-800">
+                    <div className="bg-gray-800 p-5 rounded-2xl hover:bg-gray-900">
                         <h3 className="text-white font-bold text-xl md:text-3xl lg:text-5xl">NODE JS</h3>
                     </div>
                 </Link>
